97aa9edc32bfd1254568727c720848334dd72c4c
swapnajerin <smk.1590@gmail.com>
2018-10-26 18:25:24 +0530

    Correction

--- src/android/com/plugin/gcm/GCMIntentService.java
+++ src/android/com/plugin/gcm/GCMIntentService.java
@@ -141,6 +141,10 @@ public class GCMIntentService extends GCMBaseIntentService {
     String title = "TITLE"; 
     String message = "MESSAGE"; 
     int defaults = Notification.DEFAULT_ALL;
+    Intent notificationIntent = new Intent(this, PushHandlerActivity.class);
+    notificationIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+    notificationIntent.putExtra("pushBundle", extras);
+
     PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
     NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
    NotificationCompat.Builder mBuilder =
